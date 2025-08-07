22761d428aca0cfd32a74bf9566d7f050d6b16a7
Kelly Alves Robson <kellyalverobson@gmail.com>
2025-08-07 07:36:30 -0300

    fiz funcionalides e estilizacao basicas

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -1,18 +1,26 @@
-export function NavBar(props){
+import './index.css'
+
+export function NavBar(props) {
 
     function goToPrevious() {
-        if(props.currentIndex === 0) return
-       props.setCurrentIndex(props.currentIndex -1)
+        if (props.currentIndex === 0) return
+        props.setAnswerIsVisible(false)
+        props.setCurrentIndex(props.currentIndex - 1)
     }
 
     function goToNext() {
-        if(props.currentIndex === props.lastIndex) return
-       props.setCurrentIndex(props.currentIndex +1)
+        if (props.currentIndex === props.lastIndex) return
+        props.setAnswerIsVisible(false)
+        props.setCurrentIndex(props.currentIndex + 1)
     }
 
-    return <div>
-        <button onClick={goToPrevious}>anterior</button>
-        <button>mostrar resposta</button>
-        <button onClick={goToNext}>próxima</button>
+    function flipCard() {
+        props.setAnswerIsVisible(!props.answerIsVisible)
+    }
+
+    return <div className="navbar">
+        <button onClick={goToPrevious} disabled={props.currentIndex === 0}>anterior</button>
+        <button onClick={flipCard}>mostrar {props.answerIsVisible ? 'pergunta' : 'resposta'}</button>
+        <button onClick={goToNext} disabled={props.currentIndex === props.lastIndex}>próxima</button>
     </div>
 }
